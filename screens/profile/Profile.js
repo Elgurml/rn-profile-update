09fd474f531650513254ccbc2f232a1c766e901c
Elgur <leroy.maxence@yahoo.com>
2020-04-02 12:05:41 +0200

    basic layout of profile

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 
-
-const Profile = (props) => {
-    return (
-        <View style={styles.screen}>
-            <Text>Profile page!</Text>
-        </View>
-    )
-}
+const Profile = props => {
+	return (
+		<View style={styles.screen}>
+			<Image
+				style={styles.image}
+				source={{
+					uri:
+						"https://images.unsplash.com/photo-1516328432920-123fae55f0f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+				}}
+			/>
+			<View style={styles.idCard}>
+				<Text>Tarja</Text>
+				<Text>Turunen</Text>
+				<Text>
+					I like breathing the fresh air of the fjords, it makes me
+					feel alive !
+				</Text>
+			</View>
+		</View>
+	);
+};
 
 Profile.navigationOptions = navData => {
-    return {
-        headerTitle: "Your Profile",
-        headerRight: () => (
-            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+	return {
+		headerTitle: "Your Profile",
+		headerRight: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
 				<Item
 					title="Cart"
 					iconName={
@@ -28,16 +41,32 @@ Profile.navigationOptions = navData => {
 					}}
 				/>
 			</HeaderButtons>
-        )
-    }
-}
+		)
+	};
+};
 
-export default Profile
+export default Profile;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+	screen: {
+		flex: 1,
+		alignItems: "center"
+	},
+	image: {
+		width: "100%",
+		height: "40%",
+		borderBottomLeftRadius: 20,
+		borderBottomRightRadius: 20
+    },
+    idCard: {
+        marginHorizontal: 40,
+        padding: 10,
+        shadowColor: "black",
+		shadowOpacity: 0.6,
+		shadowOffset: { width: 0, height: 20 },
+		shadowRadius: 8,
+		elevation: 5,
+		borderRadius: 10,
+		backgroundColor: "white"
     }
-})
+});
